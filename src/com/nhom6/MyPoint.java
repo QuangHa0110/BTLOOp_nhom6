4f645792ff6d5ba60d0f14c4bc3376bddc449481
Admin <Admin@DESKTOP-VUN3CO5>
2021-04-18 22:37:07 +0700

    AnPoint

--- src/com/nhom6/MyPoint.java
+++ src/com/nhom6/MyPoint.java
@@ -1,24 +1,35 @@
 package com.nhom6;
 
 import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class MyPoint extends Canvas {
 	
-	private int indexPoint;
-	private double x;
-	private double y;
-
+	//Tao diem
+	private Point2D.Double Point;
+	//So chi diem
+	private int indexPoint=0;
 	public MyPoint(double x, double y, int indexPoint) {
-		this.indexPoint = indexPoint;
-		this.x = x;
-		this.y = y;
+		Point = new Point2D.Double(x,y);
+		this.indexPoint=indexPoint;
 	}
-	
-	public void draw(Graphics2D g) {
-		g.drawOval((int) x, (int) y, 40, 40);
-		g.drawString(Integer.toString(indexPoint), (int) x + 17, (int) y + 15);
-		setForeground(Color.ORANGE);
+	public void draw(Graphics g) {
+		//Ve diem mau sac diem so chi diem
+		g.setColor(Color.BLACK);
+		//((Graphics2D) g).draw((Shape) Point);
+		g.drawOval((int)Point.getX(),(int)Point.getY(),5,5);
+		if(indexPoint>0) {
+			String s="";
+			g.drawString(s+indexPoint,(int)Point.getX(),(int)(Point.getY()+20));
+		}
+	}
+	public int getIndexPoint() {
+		return indexPoint;
+	}
+	public void setIndexPoint(int indexPoint) {
+		this.indexPoint = indexPoint;
 	}
 }
