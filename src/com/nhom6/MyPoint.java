3e35f32821232d8e39b45fb4a4741a5b52f9d82e
Viet Dung <dung2001mk123@gmail.com>
2021-04-18 22:12:41 +0700

    MyPoint

--- src/com/nhom6/MyPoint.java
+++ src/com/nhom6/MyPoint.java
@@ -1,5 +1,24 @@
 package com.nhom6;
 
-public class MyPoint {
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import java.awt.Color;
 
+public class MyPoint extends Canvas {
+	
+	private int indexPoint;
+	private double x;
+	private double y;
+
+	public MyPoint(double x, double y, int indexPoint) {
+		this.indexPoint = indexPoint;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(Graphics2D g) {
+		g.drawOval((int) x, (int) y, 40, 40);
+		g.drawString(Integer.toString(indexPoint), (int) x + 17, (int) y + 15);
+		setForeground(Color.ORANGE);
+	}
 }
