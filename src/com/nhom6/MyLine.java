e321298fd281ec247ab2ce2fa235d6db9a243028
hngo512 <hngo419512@gmail.com>
2021-04-18 23:14:05 +0700

    added class MyLine

--- src/com/nhom6/MyLine.java
+++ src/com/nhom6/MyLine.java
@@ -8,66 +8,87 @@ import java.awt.Polygon;
 
 public class MyLine {
 
+	// so chi cac diem
 	private int indexP1;
 	private int indexP2;
 	private Line2D.Double line;
-	private Color color[] = {Color.white, Color.blue};
-	private int type=0;
+	private Color color[] = { Color.white, Color.blue };
+	private int type = 0;
+	// so chi cua line
 	private int indexLine = 0;
+	// ban kinh cua hinh tron point
 	private final double radius = 15;
+
 	public MyLine(Point p1, Point p2, int indexP1, int indexP2) {
 		line = new Line2D.Double(p1, p2);
 		this.indexP1 = indexP1;
 		this.indexP2 = indexP2;
 	}
 
+	// ve line khi chi co 1 line qua 2 diem
 	public void drawLineMiddle(Graphics2D g) {
 		g.setColor(Color.black);
+		// dieu chinh do rong cua line
 		g.setStroke(new BasicStroke(2));
 		g.draw(line);
 		if (indexLine > 0) {
+			// ve so chi cua line
 			String str = "(" + indexLine + ")";
 			g.drawString(str, (int) (Math.abs(line.x1 + line.x2) / 2), (int) (line.y1 + line.y2) / 2);
 		}
-		
+
 	}
 
+	// ve line khi co 2 line qua 2 diem (2 chieu)
 	public void drawLineLeft(Graphics2D g) {
 		g.setColor(Color.black);
-	    g.setStroke(new BasicStroke(2));
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		Point p1 = new Point((int)(line.x1+(line.y1-line.y2)*radius/d),(int)(line.y1+(line.x2-line.x1)*radius/d));
-		Point p2 = new Point((int)(line.x2+(line.y1-line.y2)*radius/d),(int)(line.y2+(line.x2-line.x1)*radius/d));
-		Line2D.Double line1 = new Line2D.Double(p1,p2);
+		// dieu chinh do rong cua line
+		g.setStroke(new BasicStroke(2));
+		// range la do dai duong noi 2 point
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2));
+		// tao hai diem va line1 ben trai cua line
+		Point p1 = new Point((int) (line.x1 + (line.y1 - line.y2) * radius / range), (int) (line.y1 + (line.x2 - line.x1) * radius / range));
+		Point p2 = new Point((int) (line.x2 + (line.y1 - line.y2) * radius / range), (int) (line.y2 + (line.x2 - line.x1) * radius / range));
+		Line2D.Double line1 = new Line2D.Double(p1, p2);
 		g.draw(line1);
 		if (indexLine > 0) {
 			String str = "(" + indexLine + ")";
 			g.drawString(str, (int) (Math.abs(line.x1 + line.x2) / 2), (int) (line.y1 + line.y2) / 2);
 		}
 	}
+
+	// ve mui ten cho truong hop chi co 1 line giua 2 diem
 	public void drawArrowMiddle(Graphics2D g) {
 		g.setColor(Color.black);
-		double range = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		Line2D.Double line1= new Line2D.Double(line.x1+radius*(line.x2-line.x1)/range, line.y1+radius*(line.y2-line.y1)/range, line.x2-radius*(line.x2-line.x1)/range, line.y2-radius*(line.y2-line.y1)/range);
-		double range1 = Math.sqrt(Math.pow((line1.x2-line1.x1), 2)+ Math.pow((line1.y2-line1.y1), 2));
-		int[] x = {(int)line1.x2, (int)(line1.x2+10*(line1.x1-line1.x2)/range1+4*(line1.y2-line.y1)/range1), (int)(line1.x2+10*(line1.x1-line1.x2)/range1-4*(line1.y2-line1.y1)/range1) };
-		int[] y = {(int)line1.y2, (int)(line1.y2+10*(line1.y1-line1.y2)/range1+4*(line1.x1-line.x2)/range1), (int)(line1.y2+10*(line1.y1-line1.y2)/range1-4*(line1.x1-line1.x2)/range1) };
-		Polygon p= new Polygon(x, y, 3);
+		// range la do dai duong noi 2 point
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2));
+		// line1 la duong noi 2 diem thuoc 2 hinh tron point va gan nhau nhat
+		Line2D.Double line1 = new Line2D.Double(line.x1 + radius * (line.x2 - line.x1) / range, line.y1 + radius * (line.y2 - line.y1) / range, line.x2 - radius * (line.x2 - line.x1) / range, line.y2 - radius * (line.y2 - line.y1) / range);
+		double range1 = Math.sqrt(Math.pow((line1.x2 - line1.x1), 2) + Math.pow((line1.y2 - line1.y1), 2));
+		// 2 mang x,y la mang chua toa do 3 diem cua tam giac mui ten
+		int[] xPoint1 = { (int) line1.x2, (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range1 + 4 * (line1.y2 - line.y1) / range1), (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range1 - 4 * (line1.y2 - line1.y1) / range1) };
+		int[] yPoint1 = { (int) line1.y2, (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range1 + 4 * (line1.x1 - line.x2) / range1), (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range1 - 4 * (line1.x1 - line1.x2) / range1) };
+		// ve mui ten
+		Polygon p = new Polygon(xPoint1, yPoint1, 3);
 		g.fillPolygon(p);
-		g.drawPolygon(x, y, 3);
+		g.drawPolygon(xPoint1, yPoint1, 3);
 	}
+
+	// ve mui ten cho truong hop có 2 line giua 2 diem
 	public void drawArrowLeft(Graphics2D g) {
 		g.setColor(Color.black);
-		
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		Point p1 = new Point((int)(line.x1+(line.y1-line.y2)*radius/d),(int)(line.y1+(line.x2-line.x1)*radius/d));
-		Point p2 = new Point((int)(line.x2+(line.y1-line.y2)*radius/d),(int)(line.y2+(line.x2-line.x1)*radius/d));
-		Line2D.Double line1 = new Line2D.Double(p1,p2);
-		int[] x1 = {(int)line1.x2, (int)(line1.x2+10*(line1.x1-line1.x2)/d+5*(line1.y2-line1.y1)/d), (int)(line1.x2+10*(line1.x1-line1.x2)/d-5*(line1.y2-line1.y1)/d) };
-		int[] y1 = {(int)line1.y2, (int)(line1.y2+10*(line1.y1-line1.y2)/d+5*(line1.x1-line1.x2)/d), (int)(line1.y2+10*(line1.y1-line1.y2)/d-5*(line1.x1-line1.x2)/d) };
-		Polygon p= new Polygon(x1, y1, 3);
+		// range la do dai duong noi 2 point
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2));
+		Point p1 = new Point((int) (line.x1 + (line.y1 - line.y2) * radius / range), (int) (line.y1 + (line.x2 - line.x1) * radius / range));
+		Point p2 = new Point((int) (line.x2 + (line.y1 - line.y2) * radius / range), (int) (line.y2 + (line.x2 - line.x1) * radius / range));
+		Line2D.Double line1 = new Line2D.Double(p1, p2);
+		// 2 mang x,y la mang chua toa do 3 diem cua tam giac mui ten
+		int[] xPoint2 = { (int) line1.x2, (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range + 5 * (line1.y2 - line1.y1) / range), (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range - 5 * (line1.y2 - line1.y1) / range) };
+		int[] yPoint2 = { (int) line1.y2, (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range + 5 * (line1.x1 - line1.x2) / range), (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range - 5 * (line1.x1 - line1.x2) / range) };
+		// ve mui ten
+		Polygon p = new Polygon(xPoint2, yPoint2, 3);
 		g.fillPolygon(p);
-		g.drawPolygon(x1, y1, 3);
+		g.drawPolygon(xPoint2, yPoint2, 3);
 	}
 
 	public int getIndexP1() {
